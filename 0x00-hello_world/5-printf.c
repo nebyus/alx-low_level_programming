078d0056d70c218c5bfd7cf7fdcd7d88e3fe06d9
root <rtgneba@gmail.com>
2023-02-16 18:56:40 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ * main - Entry
+ * Return: Always
+ */
+int main(void)
+{       printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
