7a180d95d93e4c05140e1ba8a8246781e0dc99a9
root <rtgneba@gmail.com>
2023-02-21 12:25:39 -0800

    Update Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - prints the aign of a number
+ * print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 for poitive num, -1 for negative num or zero for anything else
  */
